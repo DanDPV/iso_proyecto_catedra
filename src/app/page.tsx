eb3a23c28656bdef4fb8bf6000137ee62aecb531
DanDPV <vicentedepaz.contacto@gmail.com>
2023-10-14 15:59:07 -0600

    feat(RootPage): add the list of policies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
 import type { Metadata } from 'next';
 
+import PolicyTable from '@/components/PolicyTable';
+
 export const metadata: Metadata = {
   title: 'Homepage',
 };
 
 function Page() {
-  return <div>Page</div>;
+  return (
+    <>
+      <section className="py-5">
+        <h1 className="text-center text-2xl font-bold">List of policies</h1>
+        <h2 className="text-center text-gray-600">
+          List of all the policies registered in the system.
+        </h2>
+      </section>
+      <section className="py-5">
+        <PolicyTable />
+      </section>
+    </>
+  );
 }
 
 export default Page;
